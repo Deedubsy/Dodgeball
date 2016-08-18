022350db3569c5f9d94dc9323bb0b51bbc543d08
deedub <dwormald12@gmail.com>
2016-08-18 23:09:25 +1000

    Initial Upload

---
+++ Assets/Dodgeball_Assets/Characters/Scripts/DodgeballCharacterController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DodgeballCharacterController : MonoBehaviour
+{
+
+    public float movementSpeed = 2.0f;
+    public float gravity = 7.8F;
+    private Vector3 movementDirection = Vector3.zero;
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movementDirection = transform.TransformDirection(movementDirection);
+            movementDirection *= movementSpeed;
+            //if (Input.GetButton("Jump"))
+            //    moveDirection.y = jumpSpeed;
+
+        }
+        movementDirection.y -= gravity * Time.deltaTime;
+        controller.Move(movementDirection * Time.deltaTime);
+    }
+}
