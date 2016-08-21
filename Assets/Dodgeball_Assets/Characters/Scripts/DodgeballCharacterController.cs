d7979833df1485e60598bb943ede91745ce59030
deedub <dwormald12@gmail.com>
2016-08-21 16:14:30 +1000

    update2

--- Assets/Dodgeball_Assets/Characters/Scripts/DodgeballCharacterController.cs
+++ Assets/Dodgeball_Assets/Characters/Scripts/DodgeballCharacterController.cs
@@ -6,27 +6,42 @@ public class DodgeballCharacterController : MonoBehaviour
 {
 
     public float movementSpeed = 2.0f;
+    public float range = 100.0f;
     public float gravity = 7.8F;
-    private Vector3 movementDirection = Vector3.zero;
-    CharacterController controller;
-    public GameObject ball1;
-    public GameObject ball2;
-    public GameObject ball3;
+    public Rigidbody ball1;
+    public Rigidbody ball2;
+    public Rigidbody ball3;
+    public Camera camera;
 
-    GameObject ballHeld;
+
+    private Vector3 movementDirection = Vector3.zero;
+    CharacterController character;
+
+
+
+    //public GameObject ball1;
+    //public GameObject ball2;
+    //public GameObject ball3;
+    //List<GameObject> lstOfBalls;
+    //GameObject ballHeld;
+
+    List<Rigidbody> lstOfBalls;
+
+
+    Rigidbody ballHeld;
 
     GameObject closestObj = null;
 
-    List<GameObject> lstOfBalls;
+    bool testThrow = false;
 
     // Use this for initialization
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        character = GetComponent<CharacterController>();
 
         ballHeld = null;
 
-        lstOfBalls = new List<GameObject>();
+        lstOfBalls = new List<Rigidbody>();
         lstOfBalls.Add(ball1);
         lstOfBalls.Add(ball2);
         lstOfBalls.Add(ball3);
@@ -35,7 +50,7 @@ public class DodgeballCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (controller.isGrounded)
+        if (character.isGrounded)
         {
             movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             movementDirection = transform.TransformDirection(movementDirection);
@@ -45,7 +60,7 @@ public class DodgeballCharacterController : MonoBehaviour
 
         }
         movementDirection.y -= gravity * Time.deltaTime;
-        controller.Move(movementDirection * Time.deltaTime);
+        character.Move(movementDirection * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.F))
             ballHeld = GetNearBalls();
@@ -61,34 +76,35 @@ public class DodgeballCharacterController : MonoBehaviour
 
         if (ballHeld != null)
         {
-            ballHeld.transform.position = controller.transform.position + new Vector3(0, 0, 1.5f);
+            ballHeld.transform.position = character.transform.position + new Vector3(0, 0, 1.5f);
         }
+
+        
+        Debug.DrawLine(Vector3.zero, new Vector3(0.0f, 5.0f, 0.0f), Color.red);
     }
 
-    private GameObject GetNearBalls()
+    private Rigidbody GetNearBalls()
     {
-        GameObject closestObj = null;
-        List<GameObject> lstOfTempObjects = new List<GameObject>();
+        Rigidbody closestObj = null;
+        List<Rigidbody> lstOfTempObjects = new List<Rigidbody>();
 
-        Collider[] colliders = Physics.OverlapSphere(controller.transform.position, 100, 1);
+        Collider[] colliders = Physics.OverlapSphere(character.transform.position, 100, 1);
         foreach (var collisions in colliders)
         {
-            foreach (GameObject ball in lstOfBalls)
+            foreach (Rigidbody ball in lstOfBalls)
             {
-                if (collisions.gameObject == ball)
+                if (collisions.gameObject == ball.gameObject)
                 {
                     lstOfTempObjects.Add(ball);
                 }
             }
         }
 
-        if (lstOfTempObjects != null && lstOfTempObjects.Count > 0) closestObj = new GameObject();
-
         float closestDistance = 0.0f;
 
         for (int i = 0; i < lstOfTempObjects.Count; i++)
         {
-            float diff = Vector3.Distance(lstOfTempObjects[i].transform.position, controller.transform.position);
+            float diff = Vector3.Distance(lstOfTempObjects[i].transform.position, character.transform.position);
 
             if (diff < closestDistance || closestDistance == 0.0f)
             {
@@ -102,8 +118,23 @@ public class DodgeballCharacterController : MonoBehaviour
 
     private void ThrowBall()
     {
-        Vector3 mousePos = Input.mousePosition;
+        //Vector3 force = camera.ScreenToWorldPoint(Input.mousePosition) - controller.transform.position;
 
-        //if()
+
+
+
+        //force.Normalize();
+
+        //ballHeld.AddForce(force);
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Debug.DrawLine(character.transform.position, ray.origin);
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            Debug.DrawLine(character.transform.position, hit.point);
+        }
     }
 }
